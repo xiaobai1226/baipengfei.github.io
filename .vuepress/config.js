6de18fe37416e2b61f9fc0e843cbd97ceb710130
xiaobai1226 <844042976@qq.com>
2020-10-02 02:48:39 +0800

    feat(moudle) 1、修改语言 2、修改配置文件 3、替换头像图片 4、完成简单工厂模式文章的撰写

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -22,41 +22,58 @@ module.exports = {
         ]
     ],
     "theme": "reco",
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
     "themeConfig": {
         // 导航栏
         "nav": [
             {
-                "text": "Home",
+                "text": "首页",
                 "link": "/",
                 "icon": "reco-home"
             },
             {
-                "text": "TimeLine",
+                "text": "时间轴",
                 "link": "/timeline/",
                 "icon": "reco-date"
             },
+            // {
+            //     "text": "Docs",
+            //     "icon": "reco-message",
+            //     "items": [
+            //         {
+            //             "text": "vuepress-reco",
+            //             "link": "/docs/theme-reco/"
+            //         }
+            //     ]
+            // },
             {
-                "text": "Docs",
-                "icon": "reco-message",
-                "items": [
-                    {
-                        "text": "vuepress-reco",
-                        "link": "/docs/theme-reco/"
-                    }
-                ]
-            },
-            {
-                "text": "Contact",
+                "text": "关于",
                 "icon": "reco-message",
                 "items": [
                     {
                         "text": "GitHub",
-                        "link": "https://github.com/recoluan",
+                        "link": "https://github.com/xiaobai1226",
                         "icon": "reco-github"
+                    },
+                    {
+                        "text": "码云",
+                        "link": "https://gitee.com/xiaobai1226",
+                        "icon": "reco-mayun"
+                    },
+                    {
+                        "text": "WeChat",
+                        "link": "https://github.com/xiaobai1226",
+                        "icon": "reco-wechat"
                     }
                 ]
             }
         ],
+        //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+        subSidebar: 'auto',
         // 侧边栏
         "sidebar": {
             "/docs/theme-reco/": [
@@ -73,36 +90,36 @@ module.exports = {
                 // 在导航栏菜单中所占的位置，默认2
                 "location": 2,
                 // 默认文案 “分类”
-                "text": "Category"
+                "text": "分类"
             },
             "tag": {
                 // 在导航栏菜单中所占的位置，默认3
                 "location": 3,
                 // 默认文案 “标签”
-                "text": "Tag"
+                "text": "标签"
             }
         },
         // 友情链接
         "friendLink": [
-            {
-                "title": "午后南杂",
-                "desc": "Enjoy when you can, and endure when you must.",
-                "email": "1156743527@qq.com",
-                "link": "https://www.recoluan.com"
-            },
-            {
-                "title": "vuepress-theme-reco",
-                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-                "link": "https://vuepress-theme-reco.recoluan.com"
-            }
+            // {
+            //     "title": "午后南杂",
+            //     "desc": "Enjoy when you can, and endure when you must.",
+            //     "email": "1156743527@qq.com",
+            //     "link": "https://www.recoluan.com"
+            // },
+            // {
+            //     "title": "vuepress-theme-reco",
+            //     "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+            //     "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+            //     "link": "https://vuepress-theme-reco.recoluan.com"
+            // }
         ],
         "logo": "/logo.png",
         "search": true,
         "searchMaxSuggestions": 10,
         "lastUpdated": "Last Updated",
         "author": "高压锅里的小白",
-        "authorAvatar": "/avatar.png",
+        "authorAvatar": "/avatar.jpeg",
         "record": "xxxx",
         "startYear": "2017"
     },
